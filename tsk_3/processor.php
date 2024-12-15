54fbf6494045680d744ebcea2aeb579ef7589466
Ishan Pamodya <inishan9999@gmail.com>
2024-12-15 11:27:01 +0530

    tsk_3

---
+++ tsk_3/processor.php
@@ -0,0 +1,12 @@
+
+<div style="display: flex; justify-content: center; align-items: center;">
+    <div style="display: block;">
+    
+    <?php
+        $title=$_GET['title'];
+
+        echo "Hello <b>$title</b>"
+    ?>
+
+    </div>
+</div>
